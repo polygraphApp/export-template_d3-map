defcde95bf49b0266f05b7091cc81fd84e30e34d
mhkeller <code@mhkeller.com>
2025-09-14 18:42:21 +0100

    add template

---
+++ src/routes/_template.svelte
@@ -0,0 +1,10 @@
+<script>
+	import MapD3 from '$lib/MapD3.svelte';
+	import loadConfig from '$lib/modules/loadConfig.js';
+
+	/** imports **/
+
+	const config = loadConfig(/** layers **/);
+</script>
+
+<MapD3 {config} />
